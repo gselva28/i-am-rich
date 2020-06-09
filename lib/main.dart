c29020c27fb6b3c7a038417a960c36337c251f69
GSelvakumar <gselvakumarofficial@gmail.com>
2020-06-09 13:05:56 +0530

    first app

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+//main function is the start of the program.
+//center has the child which is the text.
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(
+            child: Text("I am rich"),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
